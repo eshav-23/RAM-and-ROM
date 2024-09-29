ee089fd65450893e62ed70dbbee7e906cd8a6648
eshav-23 <128998458+eshav-23@users.noreply.github.com>
2024-09-29 17:28:26 +0530

    Update tb_ram1.v

--- tb.v
+++ tb.v
@@ -20,7 +20,62 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module tb(
+module tb();
+reg [7:0]ip;
+reg clk,wr;
+reg [5:0]add;
+wire [7:0]q;
+ram1 DUT (ip,clk,add,wr,q);
+initial
+begin
+clk=1'b1;
+forever #5clk = ~clk;
+end
 
-    );
+initial
+begin
+ip = 8'h01;
+add = 5'd0;
+wr = 1'b1;
+#10
+
+ip = 8'h02;
+add = 5'd1;
+#10
+
+ip = 8'h03;
+add = 5'd2;
+#10
+
+ip = 8'h04;
+add = 5'd3;
+#10
+
+ip = 8'h05;
+add = 5'd4;
+#10
+
+add = 5'd0;
+wr = 1'b0;
+#10
+
+add = 5'd1;
+
+#10
+
+add = 5'd2;
+#10
+
+add = 5'd3;
+#10
+
+add = 5'd4;
+#10
+ip = 8'h11;
+add = 5'd5;
+wr = 1'b1;
+#100 $finish;
+end
+
+
 endmodule
